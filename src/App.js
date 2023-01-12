0f92a8146ffe5dea25898961046cb3bb8ef0c0ab
Matt M <Afrodesiacc@gmail.com>
2023-01-11 20:19:47 -0500

    Added useEffect and State to App.js; student list visible in Browser Console.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import React from 'react';
+import StudentList from './Components/StudentList';
 import './App.css';
+import { useEffect, useState } from 'react'
+
+const API_URL = "http://localhost:9000"
 
 function App() {
+const [studentData, setStudentData] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(`${API_URL}/students`)
+      const json = await res.json()
+      console.log(json)
+      const { data } = json;
+      setStudentData(data)
+    }
+    fetchData();
+  },[] )
+
   return (
     <div className="App">
       <h1>Ayyy, bruh!</h1>
+      <StudentList/>
     </div>
   );
 }
---
+++ src/Components/StudentList.js
@@ -0,0 +1,8 @@
+import react from 'react'
+
+const StudentList = () => {
+    return (<h2>StudentList</h2>)
+}
+
+
+export default StudentList
